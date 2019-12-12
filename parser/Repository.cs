d08f737926af64e676489b6293b8c8547c93d975
jjdelorme <jjdelorme@yahoo.com>
2019-12-11 19:05:21 -0500

    Adding sample Cosmos code.

--- parser/Repository.cs
+++ parser/Repository.cs
@@ -2,10 +2,29 @@ using System;
 using System.Configuration;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.Azure.Cosmos;
 
 namespace Trucks
 {
     class Repository
     {
+        // The Azure Cosmos DB endpoint for running this sample.
+        private static readonly string EndpointUri = ConfigurationManager.AppSettings["EndPointUri"];
+
+        // The primary key for the Azure Cosmos account.
+        private static readonly string PrimaryKey = ConfigurationManager.AppSettings["PrimaryKey"];
+
+        // The Cosmos client instance
+        private CosmosClient cosmosClient;
+
+        // The database we will create
+        private Database database;
+
+        // The container we will create.
+        private Container container;
+
+        // The name of the database and container we will create
+        private string databaseId = "trucksdb";
+        private string containerId = "SettlementHistory";        
     }
 }
