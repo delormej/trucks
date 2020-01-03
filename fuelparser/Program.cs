dedcaf7bdc780e5c8e139c7dae96b749505ccea1
Jason De Lorme <jasondel@microsoft.com>
2020-01-03 15:35:39 -0500

    Read csv, create json, deserialize as objects.

--- fuelparser/Program.cs
+++ fuelparser/Program.cs
@@ -2,7 +2,7 @@
 using System.IO;
 using System.Text;
 using ChoETL;
-using System.Text.Json.Serialization;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 
 namespace FuelParser
@@ -21,26 +21,34 @@ namespace FuelParser
                 using (var w = new ChoJSONWriter(sb))
                 {
                     w.Write(p);
-                    var l = w.CastTo<List<FuelCharge>>();
                 }
             }
 
-            Console.WriteLine(sb.ToString());
+            List<FuelCharge> list = JsonConvert.DeserializeObject<List<FuelCharge>>(sb.ToString());
+            foreach (var l in list)
+                System.Console.WriteLine(l);
+
+            //Console.WriteLine(sb.ToString());
         }
     }
 
     class FuelCharge
     {
-        [JsonPropertyName("Transaction_Date")]
+        [JsonProperty("Transaction_Date")]
         public string TransactionDate { get; set; }
         
-        [JsonPropertyName("Transaction_Time")]
+        [JsonProperty("Transaction_Time")]
         public string TransactionTime { get; set; }
 
-        [JsonPropertyName("Net_Cost")]
+        [JsonProperty("Net_Cost")]
         public double NetCost { get; set; }
 
-        [JsonPropertyName("Emboss_Line_2")]
-        public int TruckId { get; set; }
+        [JsonProperty("Emboss_Line_2")]
+        public string TruckId { get; set; }
+
+        public override string ToString()
+        {
+            return $"{TruckId}, {TransactionDate}, {NetCost}";
+        }
     }
 }
