1bbf33a1d85bc3373565c8fa4058b4fa8a7148b9
jjdelorme <jjdelorme@yahoo.com>
2020-02-14 20:27:39 -0500

    Fixing unique ID for fuel charge.  Discounts entries were blanking out actual charge entry as the key wasn't unique enough.  Also fixing Week 1 Fuel Charges were missing.

--- parser.Tests/ToolsTest.cs
+++ parser.Tests/ToolsTest.cs
@@ -9,7 +9,7 @@ namespace parser.Tests
         [Fact]
         public void TestGetPayrollWeekYear()
         {
-            PrintYearWeek(DateTime.Parse("2019/12/6"));
+            PrintYearWeek(DateTime.Parse("2019/12/17"));
             
             PrintYearWeek(DateTime.Parse("2019/12/27"), "CD630340 = week 1"); 
             PrintYearWeek(DateTime.Parse("2019/12/13"), "CD629602 = week 51");
@@ -27,6 +27,25 @@ namespace parser.Tests
             int year, week;
             Tools.GetWeekNumber(settlementDate, out week, out year);
             System.Console.WriteLine($"{settlement}\t{settlementDate.ToString("yyyy-MM-dd")}, {year}, {week}");
+        }
+
+        [Fact]
+        public void TestFuelChargeId()
+        {
+            FuelCharge charge = new FuelCharge();
+            charge.TransactionDate = "1/20/2020";
+            charge.TransactionTime = "1:48:00";
+            charge.NetCost = 24.93;
+            System.Console.WriteLine($"id: {charge.id}");
+
+            FuelCharge charge2 = new FuelCharge();
+            charge2.TransactionDate = "1/20/2020";
+            charge2.TransactionTime = "1:48:01";
+            charge2.NetCost = 24.93;
+            System.Console.WriteLine($"id: {charge2.id}");
+
+            Assert.True(charge.id != charge2.id);
+
         }
     }
 }
